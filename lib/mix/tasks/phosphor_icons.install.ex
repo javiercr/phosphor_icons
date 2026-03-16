3b9a6641a6ebd7637d9922d626f7a1fa9dddca8c
Javier Cuevas <javiercr@gmail.com>
2026-03-16 21:53:27 +0100

    feat: optionally add pi- icon to core_components.ex during install

--- lib/mix/tasks/phosphor_icons.install.ex
+++ lib/mix/tasks/phosphor_icons.install.ex
@@ -22,7 +22,7 @@ defmodule Mix.Tasks.PhosphorIcons.Install do
     // for an umbrella app use the following instead
     // let baseDir = path.join(__dirname, "../../../../deps/phosphor_icons/core/raw")
     let values = {}
-    
+
     if (!fs.existsSync(baseDir)) {
       throw new Error(
         "Phosphor Icons: The directory " + baseDir + " does not exist.\\n" +
@@ -51,7 +51,7 @@ defmodule Mix.Tasks.PhosphorIcons.Install do
           .readFileSync(fullPath)
           .toString()
           .replace(/\\r?\\n|\\r/g, "")
-        
+
         return {
           [`--pi-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
           "-webkit-mask": `var(--pi-${name})`,
@@ -72,6 +72,7 @@ defmodule Mix.Tasks.PhosphorIcons.Install do
   def run(_args) do
     create_plugin_file()
     update_app_css()
+    update_core_components()
   end
 
   defp create_plugin_file do
@@ -104,7 +105,7 @@ defmodule Mix.Tasks.PhosphorIcons.Install do
       else
         new_content =
           String.trim_trailing(content) <>
-            "\n\n/* Phosphor Icons */\n" <> plugin_directive <> "\n"
+            "\\n\\n/* Phosphor Icons */\\n" <> plugin_directive <> "\\n"
 
         File.write!(app_css, new_content)
         Mix.shell().info("[:phosphor_icons] Added plugin directive to #{app_css}")
@@ -113,6 +114,65 @@ defmodule Mix.Tasks.PhosphorIcons.Install do
       Mix.shell().error(
         "[:phosphor_icons] #{app_css} does not exist. Cannot append plugin directive."
       )
+    end
+  end
+
+  defp update_core_components do
+    case Path.wildcard("lib/*_web/components/core_components.ex") do
+      [file | _] ->
+        content = File.read!(file)
+
+        if String.contains?(content, ~s(def icon(%{name: "pi-" <> _})) do
+          Mix.shell().info("[:phosphor_icons] #{file} already contains pi- icon function.")
+        else
+          # Find the heroicon function and insert the phosphor icon function after it
+          hero_icon_regex = ~r/def icon\\(%\\{name: "hero-" <> _\\} = assigns\\) do[\\s\\S]*?end/
+
+          phosphor_component = """
+
+
+            @doc \"\"\"
+            Renders a [Phosphoricon](https://phosphoricons.com/).
+
+            Phosphoricons come in six styles – thin, light, regular, bold, fill, and duotone.
+            By default, the regular style is used, but bold and duotone may
+            be applied by using the `-bold` and `-duotone` suffix.
+
+            You can customize the size and colors of the icons by setting
+            width, height, and background color classes.
+
+            Icons are extracted from the `deps/phosphor_icons` directory and bundled within
+            your compiled app.css by the plugin in `assets/vendor/phosphor_icons.js`.
+
+            ## Examples
+
+                <.icon name="pi-phosphor-logo" />
+                <.icon name="pi-fire" class="ml-1 size-3 motion-safe:animate-spin" />
+            \"\"\"
+            def icon(%{name: "pi-" <> _} = assigns) do
+              ~H\"\"\"
+              <span class={[@name, @class]} />
+              \"\"\"
+            end\
+          """
+
+          case Regex.run(hero_icon_regex, content) do
+            [match] ->
+              new_content = String.replace(content, match, match <> phosphor_component)
+              File.write!(file, new_content)
+              Mix.shell().info("[:phosphor_icons] Added pi- icon function to #{file}")
+
+            nil ->
+              Mix.shell().warning(
+                "[:phosphor_icons] Could not find the default `hero-` icon function in #{file}. You may need to add the `pi-` icon function manually."
+              )
+          end
+        end
+
+      [] ->
+        Mix.shell().warning(
+          "[:phosphor_icons] core_components.ex not found, skipping component injection."
+        )
     end
   end
 end
